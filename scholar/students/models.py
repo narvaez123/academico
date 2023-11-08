b3eb505d810295a46b63f7d5b8b591989f23a4a7
Ederson Valencia <sasaproxd8@gmail.com>
2023-11-07 20:01:50 -0500

    modelos

--- scholar/students/models.py
+++ scholar/students/models.py
@@ -1,6 +1,22 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 class User(models.Model):
-    email= models.CharField(max_length=100)
-    password= models.CharField(max_length=500)
+    email= models.CharField(max_length=100, null=False)
+    password= models.CharField(max_length=500, null=False)
+    status= models.BooleanField(null = True)
+    created_at= models.DateTimeField(default=datetime.datetime.now, null = False)
+    update_at=models.DateTimeField(default=datetime.datetime.now, null = False)
+    deleted_at=models.DateField(null = True)
 # Create your models here.
+
+class Students(models.Model):
+    code = models.CharField(max_length=50, null=False)
+    id_person = models.IntegerField(null=False)
+    status= models.BooleanField(null = True)
+    created_at= models.DateTimeField(default=datetime.datetime.now, null = False)
+    update_at=models.DateTimeField(default=datetime.datetime.now, null = False)
+    deleted_at=models.DateField(null = True)
+
+
